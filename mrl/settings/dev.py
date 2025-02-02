e4c3acc534ce659afd7466c8659b07b60acba5d2
justin emina <justin.emina@ideazlab.co.uk>
2025-02-02 10:38:40 +0000

    init

---
+++ mrl/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-ke1^bskxv==5g*=d5%b1+q$8_%69&uey64a!gjl87ei=*60+me"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
